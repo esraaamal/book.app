e126b5c3cd78f372e15688fe3740b0c50eb2e21a
esraaamal <ma.esraa@hotmail.com>
2020-04-14 23:35:26 +0300

    es

--- data/boooks.sql
+++ data/boooks.sql
@@ -1,10 +1,24 @@
 DROP TABLE IF EXISTS myBooks;
+DROP TABLE IF EXISTS addBooks;
 
 CREATE TABLE myBooks (
    id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     authors VARCHAR(255),
     image VARCHAR(255),
-    description TEXT
+    description TEXT ,
+    bookshelf VARCHAR(255),
+    ISBN VARCHAR(255)
+
+);
+
+CREATE TABLE addBooks (
+   id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    authors VARCHAR(255),
+    image VARCHAR(255),
+    description TEXT ,
+    bookshelf VARCHAR(255),
+    ISBN VARCHAR(255)
 
 );
---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+
+`use strict`;
+$( document ).ready(function(){
+
+$('#myForm').hide();
+$('.nav').on('click' ,function(){
+    $('#myForm').toggle();
+})
+
+
+
+});
